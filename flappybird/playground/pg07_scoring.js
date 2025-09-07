89c031a237e7a3efe513bed55fe999d99efe7d89
WarrenAndTheTwins <warrenasks@gmail.com>
2025-09-07 04:19:34 +0000

    Sep 7, 2025, 4:19 AM

--- flappybird/playground/pg07_scoring.js
+++ flappybird/playground/pg07_scoring.js
@@ -181,4 +181,5 @@ function drawScore(x, y, score, digitWidth, digitHeight){
     scoreDigits.removeAll();
     let scoreStr = str(score);
     let totalWidth = scoreStr.length * digitWidth;
+    let s
 }
