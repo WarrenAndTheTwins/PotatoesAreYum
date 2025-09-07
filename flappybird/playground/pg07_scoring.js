843f3dd768b8e87433f4ae611342a9a904a31d10
WarrenAndTheTwins <warrenasks@gmail.com>
2025-09-07 03:54:30 +0000

    Sep 7, 2025, 3:54 AM

--- flappybird/playground/pg07_scoring.js
+++ flappybird/playground/pg07_scoring.js
@@ -15,6 +15,7 @@ let gameOverLabel; // variable for sprite
 
 
 function preload(){
+    
     startScreenImage = loadImage('assets/message.png')
     bg = loadImage('assets/background-day.png');
     base = loadImage('assets/base.png');
