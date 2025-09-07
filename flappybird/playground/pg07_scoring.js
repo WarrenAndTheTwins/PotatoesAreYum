aed43bedd891fbb7f995b40fea62a1b0069e0b8d
WarrenAndTheTwins <warrenasks@gmail.com>
2025-09-07 04:02:43 +0000

    Sep 7, 2025, 4:02 AM

--- flappybird/playground/pg07_scoring.js
+++ flappybird/playground/pg07_scoring.js
@@ -61,7 +61,7 @@ function setup(){
 
     // setup the group for pipe
     pipeGroup = new Group();
-
+scoreDigits 
 startScreenLabel = new Sprite(width/2, height/2, 50, 50, 'none' )
 startScreenLabel.img = startScreenImage
 }
