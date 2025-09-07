9d6eb6647486ca564c89e8c51b32f4b4af660f8a
WarrenAndTheTwins <warrenasks@gmail.com>
2025-09-07 03:27:56 +0000

    Sep 7, 2025, 3:27 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -54,7 +54,7 @@ function setup(){
     // setup the group for pipe
     pipeGroup = new Group();
 
-startScreenLabel = new Sprite(width/2, height/2, )
+startScreenLabel = new Sprite(width/2, height/2, 50, 50, 'none' )
 }
 
 function draw(){
