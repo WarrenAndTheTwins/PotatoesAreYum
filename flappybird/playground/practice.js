c7aec938ea21c4c4aa651241c932bb1c51bf63e7
WarrenAndTheTwins <warrenasks@gmail.com>
2025-09-14 03:26:47 +0000

    Sep 14, 2025, 3:26 AM

--- flappybird/playground/practice.js
+++ flappybird/playground/practice.js
@@ -4,8 +4,9 @@ let Dave;
 function setup(){
     new Canvas (800, 400);
     background(220);
-}
-function draw(){
     Square = new Sprite(400, 200, 80, 80);
     Circle = new Sprite(100, 350, 40);
+}
+function draw(){
+    
 }
