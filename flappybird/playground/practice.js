35486975e796a26603aa37f6e627623e6a4bc593
WarrenAndTheTwins <warrenasks@gmail.com>
2025-09-14 03:36:30 +0000

    Sep 14, 2025, 3:36 AM

--- flappybird/playground/practice.js
+++ flappybird/playground/practice.js
@@ -18,6 +18,6 @@ function draw(){
         count++
     }
     if (mouse.presses ('left') || KeyboardEvent.presses('space'));
-        Richie_the_Circle = new Sprite(mouse)
+        Richie_the_Circle = new Sprite(mouse.x, mouse.y, )
 
 }
