ee9efa292a96f8925f272ebc4d09633a6a3fc445
WarrenAndTheTwins <warrenasks@gmail.com>
2025-09-14 04:12:20 +0000

    Sep 14, 2025, 4:12 AM

--- flappybird/playground/pg07_scoring.js
+++ flappybird/playground/pg07_scoring.js
@@ -119,9 +119,11 @@ function draw(){
         let pipeRightEdge = pipe.x  + pipe.w /2;//specify right most      
         // nice drawing
         let birdLeftEdge = bird.x - bird.w/2;
-        
-    
-    
+        if ( pipeRightEdge < birdLeftEdge && pipe.passed ==false){
+            pipe.passed = true;
+            score ++;
+        }
+
     }
     }
 
