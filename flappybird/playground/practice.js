4f784732f0d7917fcfc28667f978619afcbb8414
WarrenAndTheTwins <warrenasks@gmail.com>
2025-09-14 03:42:10 +0000

    Sep 14, 2025, 3:42 AM

--- flappybird/playground/practice.js
+++ flappybird/playground/practice.js
@@ -22,9 +22,9 @@ function draw(){
         Richie_the_Circle.x = mouse.x;
         Richie_the_Circle.y = mouse.y;
         Richie_the_Circle.diameter = 50;
-        Richie_the_Circle.collider = "dynamic"};
+        Richie_the_Circle.collider = "dynamic";
         Richie_the_Circle.img = popimage;
-
+    }
 }
 function preload(){
     popimage = loadimage
