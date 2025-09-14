46d59192dbc2de6139af1bd0d0691427c6d642aa
WarrenAndTheTwins <warrenasks@gmail.com>
2025-09-14 03:56:24 +0000

    Sep 14, 2025, 3:56 AM

--- flappybird/playground/pg07_scoring.js
+++ flappybird/playground/pg07_scoring.js
@@ -191,7 +191,7 @@ function drawScore(x, y, score, digitWidth, digitHeight){
     moveGroup(scoreDigits, camera.x, 24)
 }
 
-function moveGroup(group, targetX,spacing){
+function moveGroup(group, targetX, spacing){
     let totalWidth = (group.length - 1) * spacing;
-    let startX
+    let startX = (targetX)
 }
