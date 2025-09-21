569de907d337f6b328dbb16052abb63ca611fbe0
WarrenAndTheTwins <warrenasks@gmail.com>
2025-09-21 04:03:34 +0000

    Sep 21, 2025, 4:03 AM

--- fruitninja/frootnynjuh02.js
+++ fruitninja/frootnynjuh02.js
@@ -6,7 +6,7 @@ function preload(){
     dojoBG = loadImage('assets/dojobackground.png');
     let peach;
     let watermelon;
-    
+    peach = loadIma
 }
 function setup(){
     new Canvas(800, 600);
