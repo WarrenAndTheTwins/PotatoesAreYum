4986ce7e827df8c674fd1d0fbd09c63c3d68ff8f
WarrenAndTheTwins <warrenasks@gmail.com>
2025-10-05 03:26:20 +0000

    Oct 5, 2025, 3:26 AM

--- fruitninja/frootnynjuh02.js
+++ fruitninja/frootnynjuh02.js
@@ -1,42 +1,46 @@
-let dojoBG;
 let fruitGroup;
-let fruitType = [];
-
+let fruitTypes =[];
+let dogoBG;
 function preload(){
-    dojoBG = loadImage('assets/dojobackground.png');
+     dogoBG = loadImage('assets/dojobackground.png');
+    //declare peach 
     let peach = {
         whole:loadImage('assets/peachwhole.png')
     };
     let watermelon = {
-        whole:loadImage('assets/watermelonwhole.png')
-    };
-    fruitType = [peach, watermelon]
+        whole: loadImage('assets/watermelonwhole.png')
+    }
+    //store the fruit objject into fruitTypes array[]
+    fruitTypes = [peach,watermelon];
 }
 function setup(){
-    new Canvas(800, 600);
+    new Canvas(800,600);
+    //set gravity to 10
     world.gravity.y = 10;
+    fruitGroup= new Group();
 }
 function draw(){
     clear();
-    image(dojoBG, 0, 0, height, width);
-    if (frameCount % 120 == 0);
-        spawnFruit;
+    image(dogoBG,0,0,width,height);
+    // % refers to the remainder so if framecount / by 120 and the remainder is 0 then i call spawnfruit function
+    if(frameCount % 120 === 0){
+        spawnFruit();
+    }
+    if(mouse.pressing()){
+        trail = new Sprite(mouse.x,mouse.y,7);
+        trail.collider = 'none';
+        trail.color ="red";
+        trail.life = 10;
+    }
 }
+
 function spawnFruit(){
-    let fruitData = random(fruitType);
-    let RandomX = random(300, 500);
-    let fruit = fruitGroup.Sprite(RandomX, height + 20, 40);
+    let fruitData = random(fruitTypes);
+    let randomX = random(300,500);
+    let fruit = new fruitGroup.Sprite(randomX,600+20,40); 
     fruit.image = fruitData.whole;
     fruit.type = fruitData;
-    fruit.vel.y = random(-10, -14);
-    fruit.vel.x= random(-2, 2);
+    fruit.vel.y = random(-10,-14);
+    fruit.vel.x = random(-2,2);
     fruit.friction = 0;
-
-    if (mouse.pressing()){
-        trail = new Sprite(mouse.x, mouse.y, 7);
-        trail.collider = 'none';
-        trail.color = "red";
-        trail.life = 10;
-}
-
 }
