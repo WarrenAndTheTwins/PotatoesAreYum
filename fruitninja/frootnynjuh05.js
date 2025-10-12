90470ee46a51a58f46c6b64460a289398aa50b50
WarrenAndTheTwins <warrenasks@gmail.com>
2025-10-12 03:50:45 +0000

    Oct 12, 2025, 3:50 AM

--- fruitninja/frootnynjuh05.js
+++ fruitninja/frootnynjuh05.js
@@ -9,7 +9,7 @@ let slicesound;
 
 function preload(){
      dogoBG = loadImage('assets/dojobackground.png');
-     
+     sliceSound = loadSound()
     //declare peach 
     let peach = {
         whole:loadImage('assets/peachwhole.png'),
