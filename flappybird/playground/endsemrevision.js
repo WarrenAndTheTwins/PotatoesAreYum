5a9737c1014367f1fb87cfaae1225511f0426bab
WarrenAndTheTwins <warrenasks@gmail.com>
2025-10-19 03:35:45 +0000

    Oct 19, 2025, 3:35 AM

--- flappybird/playground/endsemrevision.js
+++ flappybird/playground/endsemrevision.js
@@ -10,10 +10,11 @@ background = new Canvas(500, 700)
 square = new Sprite();
 square.x = width/2;
 square.y = length/2;
+square.collider = "dynamic";
 }
 function draw(){
 image(background,0,0,width,height);
 if (mouse.presses()){
-    
+    square.
 }
 }
