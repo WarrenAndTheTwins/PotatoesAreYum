b99af0b7df76d5d63b8e2b1334d6cfd5ed105ed3
WarrenAndTheTwins <warrenasks@gmail.com>
2025-10-19 03:26:15 +0000

    Oct 19, 2025, 3:26 AM

--- flappybird/playground/endsemrevision.js
+++ flappybird/playground/endsemrevision.js
@@ -7,12 +7,10 @@ background = loadImage('assets/background-day.png');
 square = loadImage('assets/redbird-midflap.png');
 }
 function setup(){
-background = new Canvas(width, length)
+background = new Canvas(500, 700)
 square = new Sprite
 square.x = width/2
 square.y = length/2
 }
 function draw(){
-width = 500;
-length = 700;
 }
